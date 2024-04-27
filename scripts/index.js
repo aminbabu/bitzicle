7c03330552a9bca1a4812bd0ac2e51af7dc21c34
haquesomrat <ihsomrat.official@gmail.com>
2024-04-27 19:34:00 +0600

    fixed some sticky issue in index js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,3 @@
-// navbar sticky
-
-window.onscroll = function () {
-  myFunction();
-};
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 // Clear all checked price filter
 
 document
