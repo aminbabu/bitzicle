59819a338163dfe036c190843a383f9edc7973f6
haquesomrat <ihsomrat.official@gmail.com>
2024-04-27 19:25:08 +0600

    added functionalities to clear price and category filter

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,29 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// Clear all checked price filter
+
+document
+  .querySelector(".clear-all-btn-price")
+  .addEventListener("click", function () {
+    var radioButtons = document.querySelectorAll(
+      '.radio-list input[type="radio"]'
+    );
+    radioButtons.forEach(function (radioButton) {
+      radioButton.checked = false;
+    });
+  });
+
+// Clear all checked category filter
+
+document
+  .querySelector(".clear-all-btn-category")
+  .addEventListener("click", function () {
+    var checkboxes = document.querySelectorAll(
+      '.category-list input[type="checkbox"]'
+    );
+    checkboxes.forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+  });
