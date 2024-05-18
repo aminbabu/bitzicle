2d98f78365bd22cb450db9114a32ef973fe5db6e
Amin Babu <amin.babu.bd@gmail.com>
2024-05-18 07:17:30 +0600

    Refactor category filter and sub-category collapse functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,58 @@
-// Clear all checked price filter
+// global object
+const BIZTICLE = {};
 
-document
-  .querySelector(".clear-all-btn-price")
-  .addEventListener("click", function () {
-    var radioButtons = document.querySelectorAll(
-      '.radio-list input[type="radio"]'
-    );
-    radioButtons.forEach(function (radioButton) {
-      radioButton.checked = false;
+// clear all checked category filter
+BIZTICLE.clearAllCategoryFilter = function () {
+  const triggerButtons = Array.from(document.querySelectorAll(".clear-all-btn-price"));
+
+  // check if triggerButtons is empty
+  if (!triggerButtons.length) {
+    return;
+  }
+
+  triggerButtons.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const radioButtons = Array.from(document.querySelectorAll('.radio-list input[type="radio"]'));
+
+      radioButtons.forEach((radioButton) => {
+        radioButton.checked = false;
+      });
     });
-  });
+  }
+  );
+};
 
 // hide other sub-category when one is open
+BIZTICLE.hideOtherSubCategory = function () {
+    // Get all category items
+    const categoryItems = document.querySelectorAll(".cateogory-item");
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Get all category items
-  const categoryItems = document.querySelectorAll(".cateogory-item");
+    // Check if categoryItems is empty
+    if (!categoryItems.length) {
+      return;
+    }
 
-  // Iterate through each category item
-  categoryItems.forEach(function (item) {
-    // Add click event listener to category item's link
-    item
-      .querySelector(".category-item-content")
-      .addEventListener("click", function () {
-        // Get the collapse element associated with this category item
-        const collapse = item.querySelector(".collapse");
-        console.log(collapse);
+    // Iterate through each category item
+    categoryItems.forEach(function (item) {
+      // Add click event listener to category item's link
+      item
+        .querySelector(".category-item-content")
+        .addEventListener("click", function () {
+          // Get the collapse element associated with this category item
+          const collapse = item.querySelector(".collapse");
 
-        // Close all collapse elements except this one
-        document.querySelectorAll(".collapse").forEach(function (c) {
-          if (c !== collapse) {
-            c.classList.remove("show");
-          }
+          // Close all collapse elements except this one
+          document.querySelectorAll(".collapse").forEach(function (c) {
+            if (c !== collapse) {
+              c.classList.remove("show");
+            }
+          });
         });
-      });
-  });
+    });
+};
+
+// document ready
+document.addEventListener("DOMContentLoaded", function () {
+  BIZTICLE.clearAllCategoryFilter();
+  BIZTICLE.hideOtherSubCategory();
 });
