80cd3402b220d5c64e57f70648057a4dfb67d695
haquesomrat <ihsomrat.official@gmail.com>
2024-08-07 17:59:28 +0600

    add login page

--- scripts/multi-step.js
+++ scripts/multi-step.js
@@ -1,65 +1,3 @@
-// global object
-const BIZTICLE = {};
-
-// clear all checked category filter
-BIZTICLE.clearAllCategoryFilter = function () {
-  const triggerButtons = Array.from(
-    document.querySelectorAll(".clear-all-btn-price")
-  );
-
-  // check if triggerButtons is empty
-  if (!triggerButtons.length) {
-    return;
-  }
-
-  triggerButtons.forEach((btn) => {
-    btn.addEventListener("click", function () {
-      const radioButtons = Array.from(
-        document.querySelectorAll('.radio-list input[type="radio"]')
-      );
-
-      radioButtons.forEach((radioButton) => {
-        radioButton.checked = false;
-      });
-    });
-  });
-};
-
-// hide other sub-category when one is open
-BIZTICLE.hideOtherSubCategory = function () {
-  // Get all category items
-  const categoryItems = document.querySelectorAll(".cateogory-item");
-
-  // Check if categoryItems is empty
-  if (!categoryItems.length) {
-    return;
-  }
-
-  // Iterate through each category item
-  categoryItems.forEach(function (item) {
-    // Add click event listener to category item's link
-    item
-      .querySelector(".category-item-content")
-      .addEventListener("click", function () {
-        // Get the collapse element associated with this category item
-        const collapse = item.querySelector(".collapse");
-
-        // Close all collapse elements except this one
-        document.querySelectorAll(".collapse").forEach(function (c) {
-          if (c !== collapse) {
-            c.classList.remove("show");
-          }
-        });
-      });
-  });
-};
-
-// document ready
-document.addEventListener("DOMContentLoaded", function () {
-  BIZTICLE.clearAllCategoryFilter();
-  BIZTICLE.hideOtherSubCategory();
-});
-
 // Multi step form
 $(document).ready(function () {
   var currentStep = 0;
@@ -175,7 +113,7 @@ function progressHandler(event) {
 
 function completeHandler(event) {
   _("status").innerHTML = event.target.responseText;
-  // _("resumeProgressBar").value = 0;
+  _("resumeProgressBar").value = 0;
   //wil clear progress bar after successful upload
 }
 
